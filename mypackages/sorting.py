6cc5b058ecafa92bd6cf3828811a87d353c92d87
Mantahli Mofokeng <mofokenganna21@gmail.com>
2019-03-20 14:31:08 +0200

    'comment'

--- mypackages/sorting.py
+++ mypackages/sorting.py
@@ -6,41 +6,13 @@ def bubble_sort(items):
     for i in range(len(out)):
         for j in range(len(out)-1-i):
             if out[j] > out[j+1]:
-                out[j], out[j+1] = out[j+1], out[j]     # Swap!
-
-    return out
-
+                out[j], out[j+1] = out[j+1], out[j]     # Swap
+        return out
 
 
 
-def merge_sort(items):
-  """ Return array of items, sorted in ascending order """
-  if len(items) >1:
-      mid = len(items)//2 #Finding the mid of the array
-      L = items[:mid] # Dividing the array elements
-      R = items[mid:] # into 2 halves
-      merge_sort(L) # Sorting the first half
-      merge_sort(R) # Sorting the second half
-      i = j = k = 0
-      # Copy data to temp arrays L[] and R[]
-      while i < len(L) and j < len(R):
-          if L[i] < R[j]:
-              items[k] = L[i]
-              i+=1
-          else:
-              items[k] = R[j]
-              j+=1
-          k+=1
-      # Checking if any element was left
-      while i < len(L):
-          items[k] = L[i]
-          i+=1
-          k+=1
-      while j < len(R):
-          items[k] = R[j]
-          j+=1
-          k+=1
-  return items
+
+
 
 
 
